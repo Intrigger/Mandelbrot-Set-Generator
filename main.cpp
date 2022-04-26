4e4e2e7435ae44be53dd633f244ab12f5fe97890
Daniel Kushko <xintrigger@gmail.com>
2022-04-26 14:14:39 +0300

    Added user interface. Now user can run the program and draw a picture.

--- main.cpp
+++ main.cpp
@@ -10,7 +10,7 @@ struct complex_number{
 
 bool check_mandelbrot(complex_number c, int iteration){
 	complex_number z0 = {0, 0};
-	int iterations = 1000;
+	int iterations = 100;
 	for (int i = 0; i < iterations; i++){
 		z0 = {z0.real * z0.real - z0.imaginary * z0.imaginary + c.real, 2 * z0.real * z0.imaginary + c.imaginary};
 	}
@@ -28,48 +28,51 @@ int main(){
 
 	using std::cout;
 	using std::endl;
+	using std::cin;
 	using namespace graphics;
 
 	BMPImage img;
 
-	int width = 900;
-	int height = 900;
-
-
+	int width = 1024;
+	int height = 1024;
 
 	double centerx = -0.75;
 	double centery = 0.0;
-	double real_width = 1.0;
-	double real_height = 1.0;
+	double real_width = 2.0;
+	double real_height = 2.0;
 
-	double c = 0.01;
+	system("clear");
 
-	int images_number = 60*60;
+	cout << "Welcome!\n"
+		 << "This program can draw a Madnelbrot`s set and save it`s picture to a .bmp file.\n";
+	cout << "First of all, enter image width (px): ";
+	cin >> width;
 
-	for (int t = 0; t < images_number; t++){
+	cout << "Secondly, enter the image height (px): ";
+	cin >> height;
 
-		img.set_height(height); 
-		img.set_width(height);
+	cout << "Enter the real width of the image (it real size on the plane of numbers): ";
+	cin >> real_width;
 
-		for (int y = 0; y < height; y++){
-			for (int x = 0; x < width; x++){
-				float fx = (centerx - (real_width / 2.0) + (double(x) / double(width) * real_width));
-				float fy = (centery - (real_height / 2.0) + (double(y) / double(height) * real_height));
-				if (check_mandelbrot({fx, fy}, t)){
-					img.set_pixel(x, y, {0, 0, 0});
-				}
-				
+	cout << "And finally enter the real height of the image: ";
+	cin >> real_height;
+
+
+	img.set_height(height); 
+	img.set_width(width);
+
+	for (int y = 0; y < height; y++){
+		for (int x = 0; x < width; x++){
+			double fx = (centerx - (real_width / 2.0) + ((double(x) / double(width)) * real_width));
+			double fy = (centery - (real_height / 2.0) + ((double(y) / double(height)) * real_height));
+			if (check_mandelbrot({fx, fy}, 1)){
+				img.set_pixel(x, y, {0, 0, 0});
 			}
+			
 		}
-
-		real_width *= 0.9;
-		real_height *= 0.9;
-
-		cout << "Image #" << t + 1 << endl;
-
-		img.save_to_file("pictures/mandelbrot_set_" + std::to_string(t + 1) + ".bmp");
-		img.clear();
 	}
+
+	img.save_to_file("pictures/mandelbrot_set.bmp");
 
 	
 
